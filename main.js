a22789260ba8c82e61878332916a5557c16fda0c
gchyoghly <gchyoghly@gmail.com>
2023-12-15 23:01:47 -0500

    Moved Script to main.js plus update css

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
+    var panel = this.nextElementSibling.classList.toggle("open");
   });
 }
