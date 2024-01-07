9d6beb4cbd544b1e32c63226d5d9a430afc8dee6
gchyoghly <gchyoghly@gmail.com>
2024-01-06 20:51:17 -0500

    Modified Accordions to alternate

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
+var accL = document.getElementsByClassName("accordionLeft");
+var j;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var panel = this.nextElementSibling.classList.toggle("open");
+  });
+}
+
+for (j = 0; j < accL.length; j++) {
+  accL[j].addEventListener("click", function() {
+    this.classList.toggle("activeLeft");
+    var panelLeft = this.nextElementSibling.classList.toggle("open");
   });
 }
